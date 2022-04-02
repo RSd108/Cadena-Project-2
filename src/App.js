ed76ab4fbe4d7dfed61f39fcb6ca9f0378a9b194
RSd108 <rakesh@bharuth.za.net>
2022-04-02 22:46:32 +0200

    css changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import { useState, useEffect } from 'react';
 import { ethers, utils } from "ethers";
 import abi from "./contracts/RadhaCoin.json";
@@ -150,25 +151,25 @@ function App() {
   })
 
   return (
-    <main className="main-container">
-      <h2 className="headline">
-        <span className="headline-gradient">Radha Coin Project</span>
-        <img className="inline p-3 ml-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvCljbP1OzBZykt3AAls_7OlDJ0goe5nxATw&usqp=CAU" alt="Radha Coin" width="60" height="30" />
+    <main className="App">
+      <h2 className="App-header">
+        <span>Radha Coin Project
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvCljbP1OzBZykt3AAls_7OlDJ0goe5nxATw&usqp=CAU" alt="Radha Coin" width="60" height="30" />
+        </span>
       </h2>
-      <section className="customer-section px-10 pt-5 pb-10">
-        {error && <p className="text-2xl text-red-700">{error}</p>}
-        <div className="mt-5">
-          <span className="mr-5"><strong>Coin:</strong> {tokenName} 
-          <img className="inline p-3 ml-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvCljbP1OzBZykt3AAls_7OlDJ0goe5nxATw&usqp=CAU" alt="Radha Coin" width="30" height="15" />
+      <section>
+        {error && <p>{error}</p>}
+        <div>
+          <span><strong>Coin:</strong> {tokenName} 
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvCljbP1OzBZykt3AAls_7OlDJ0goe5nxATw&usqp=CAU" alt="Radha Coin" width="30" height="15" />
           </span>
-          <span className="mr-5"><strong>Ticker:</strong>  {tokenSymbol} </span>
-          <span className="mr-5"><strong>Total Supply:</strong>  {tokenTotalSupply}</span>
+          <span><strong>Ticker:</strong>  {tokenSymbol} </span>
+          <span><strong>Total Supply:</strong>  {tokenTotalSupply}</span>
         </div>
-        <div className="mt-7 mb-9">
-          <form className="form-style">
+        <div>
+          <form>
             <input
               type="text"
-              className="input-double"
               onChange={handleInputChange}
               name="walletAddress"
               placeholder="Wallet Address"
@@ -176,38 +177,34 @@ function App() {
             />
             <input
               type="text"
-              className="input-double"
               onChange={handleInputChange}
               name="transferAmount"
               placeholder={`0.0000 ${tokenSymbol}`}
               value={inputValue.transferAmount}
             />
             <button
-              className="btn-purple"
               onClick={transferToken}>Transfer Tokens</button>
           </form>
         </div>
         {isTokenOwner && (
           <section>
-            <div className="mt-10 mb-10">
-              <form className="form-style">
+            <div>
+              <form>
                 <input
                   type="text"
-                  className="input-style"
                   onChange={handleInputChange}
                   name="burnAmount"
                   placeholder={`0.0000 ${tokenSymbol}`}
                   value={inputValue.burnAmount}
                 />
                 <button
-                  className="btn-purple"
                   onClick={burnTokens}>
                   Burn Tokens
                 </button>
               </form>
             </div>
-            <div className="mt-10 mb-10">
-              <form className="form-style">
+            <div>
+              <form>
                 <input
                   type="text"
                   className="input-style"
@@ -217,7 +214,6 @@ function App() {
                   value={inputValue.mintAmount}
                 />
                 <button
-                  className="btn-purple"
                   onClick={mintTokens}>
                   Mint Tokens
                 </button>
@@ -225,15 +221,15 @@ function App() {
             </div>
           </section>
         )}
-        <div className="mt-5">
+        <div>
           <p><span className="font-bold">Contract Address: </span>{contractAddress}</p>
         </div>
-        <div className="mt-5">
+        <div>
           <p><span className="font-bold">Token Owner Address: </span>{tokenOwnerAddress}</p>
         </div>
-        <div className="mt-5">
+        <div>
           {isWalletConnected && <p><span className="font-bold">Your Wallet Address: </span>{yourWalletAddress}</p>}
-          <button className="btn-connect" onClick={checkIfWalletIsConnected}>
+          <button onClick={checkIfWalletIsConnected}>
             {isWalletConnected ? "Wallet Connected 🔒" : "Connect Wallet 🔑"}
           </button>
         </div>
